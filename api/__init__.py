afd620bf3e9ba3704f806f6edb3c0b8caf9de30f
CaioMarini <caioferrazmarini123@gmail.com>
2020-11-24 18:44:48 -0300

    Configuração Rota

---
+++ api/__init__.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+from flask import Blueprint, Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+from flask_restx import Api
+from flask_marshmallow import Marshmallow
+from flask_restx.apidoc import apidoc
+from .config import *
+
+# Definir o endpoint base da API
+URL_PREFIX = '/api'
+apidoc.url_prefix = URL_PREFIX
+
+# Instanciar o Flask
+app = Flask(__name__)
+
+# Instanciar o Database com SQLAlchemy
+db = SQLAlchemy()
+
+# Instanciar
+ma = Marshmallow(app)
+
+# Instanciar o CORS
+cors = CORS(app)
+CORS(app, resources=r'/api/*')
+
+# Instanciar o Blueprint: modularização da Api
+blueprint = Blueprint('/api',__name__,url_prefix=URL_PREFIX)
+
+# Instanciar o Flask-RestX
+api = Api(
+    version = '1.0',
+    title = 'Locadora de Equipamentos',
+    description = 'Este é um serviço para locação de equipamentos'
+)
+
+configurar_app(app)
+configurar_restx(app)
+configurar_banco(app,db)
